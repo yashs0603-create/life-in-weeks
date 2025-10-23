69461789479957e72bf5323f8ea6872aaa842bda
yashs0603-create <yashs0603@gmail.com>
2025-10-23 19:33:14 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,6 @@
+#Calculating how much time is left
+age=int(input("What is your current age?"))
+days=365*90-365*age
+weeks=52*90-52*age
+months=12*90-12*age
+print(f"You have {days} days, {weeks} weeks and {months} months left.")
